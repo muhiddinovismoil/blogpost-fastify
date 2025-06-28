f2e632719509e1ae7ef041a65ae8dcd2fd67d703
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-06-28 12:29:19 +0500

    auth register and login schema validator for request and response successfully finished

--- src/routes/auth/auth.schema.js
+++ src/routes/auth/auth.schema.js
@@ -1,5 +1,114 @@
+//
+// ********************************************************** Auth REGISTER
+//
 export const registerUserSchema = {
   body: {
     type: 'object',
+    required: ['email', 'password'],
+    properties: {
+      fullname: { type: string },
+      email: { type: 'string', pattern: '^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$' },
+      password: { type: 'string', minLength: 8 },
+    },
+  },
+  response: {
+    201: {
+      type: 'object',
+      properties: {
+        statusCode: { type: 'integer', default: 201 },
+        message: { type: 'string' },
+      },
+    },
+    400: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'integer',
+          default: 400,
+        },
+        message: { type: 'string' },
+      },
+    },
+    500: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'integer',
+          default: 500,
+        },
+        message: { type: 'string' },
+      },
+    },
+  },
+};
+//
+// ********************************************************** Auth LOGIN
+//
+export const loginUserSchema = {
+  body: {
+    type: 'object',
+    required: ['email', 'password'],
+    properties: {
+      email: { type: 'string', pattern: '^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$' },
+      password: { type: 'string', minLength: 8 },
+    },
+  },
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        statusCode: { type: 'integer', default: 200 },
+        message: { type: 'string' },
+        data: {
+          type: 'object',
+          properties: {
+            accessToken: { type: 'string' },
+            refreshToken: { type: 'string' },
+          },
+        },
+      },
+    },
+    400: {
+      type: 'object',
+      properties: {
+        statusCode: { type: 'integer', default: 400 },
+        message: { type: 'string' },
+        data: {
+          type: 'object',
+          properties: {
+            accessToken: { type: 'string' },
+            refreshToken: { type: 'string' },
+          },
+        },
+      },
+    },
+    403: {
+      type: 'object',
+      properties: {
+        statusCode: { type: 'integer' },
+        message: { type: 'string' },
+        data: {
+          type: 'object',
+          properties: {
+            accessToken: { type: 'string' },
+            refreshToken: { type: 'string' },
+          },
+        },
+      },
+    },
+    500: {
+      type: 'object',
+      properties: {
+        statusCode: { type: 'integer' },
+        message: { type: 'string' },
+        data: {
+          type: 'object',
+          properties: {
+            accessToken: { type: 'string' },
+            refreshToken: { type: 'string' },
+          },
+        },
+      },
+    },
   },
 };
