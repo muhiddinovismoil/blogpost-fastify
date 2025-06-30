0a9b0af9969a56c88032fdd74c44823b4d0c8bfe
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-06-30 20:45:22 +0500

    a little change on exporting and importing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import Fastify from 'fastify';
 import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUi from '@fastify/swagger-ui';
-import * as config from './config/index.js';
 import prismaPlugin from './plugins/db.js';
-import { mainRoutes } from './routes/index.js';
+import mainRoutes from './routes/index.js';
+import * as config from './config/index.js';
 
 const app = Fastify(config.pinoConf);
 await app.register(prismaPlugin);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import userRoutes from './user.routes.js';
 import blogsRoutes from './blogs.routes.js';
 import fileRoutes from './file.routes.js';
 
-export async function mainRoutes(fastify, opts) {
+export default async function mainRoutes(fastify, opts) {
   fastify.register(authRoutes, { prefix: '/auth' });
   fastify.register(blogsRoutes, { prefix: '/blogs' });
   fastify.register(userRoutes, { prefix: '/users' });
