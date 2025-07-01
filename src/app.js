f6edf59e36350046e51b074ca0e7738f3cec20c0
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-01 10:43:53 +0500

    jwt configuartion and jwt plugin has been added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUi from '@fastify/swagger-ui';
 import prismaPlugin from './plugins/db.js';
 import mainRoutes from './routes/index.js';
+import jwtPlugin from './plugins/jwt.js';
 import * as config from './config/index.js';
 
 const app = Fastify(config.pinoConf);
@@ -10,5 +11,6 @@ await app.register(prismaPlugin);
 await app.register(fastifySwagger, config.swaggerMainConf);
 await app.register(fastifySwaggerUi, config.swaggerExtraConf);
 await app.register(mainRoutes, { prefix: '/api/v1' });
+await app.register(jwtPlugin);
 
 export default app;
