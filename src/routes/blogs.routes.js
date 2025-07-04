f65d2791545647c6cc384fa7d4879c8dd80aa01c
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-04 22:27:10 +0500

    blog route updated

--- src/routes/blogs.routes.js
+++ src/routes/blogs.routes.js
@@ -10,7 +10,7 @@ import {
 export default async function blogsRoutes(fastify, opts) {
     fastify.get('', { ...GetAllBlogsSchema }, blogsController.getAllBlogs);
     fastify.get('/:id', { ...GetByIdBlogSchema }, blogsController.getBlogById);
-    fastify.post('/', { ...CreateBlogSchema }, blogsController.createData);
+    fastify.post('', { ...CreateBlogSchema }, blogsController.createData);
     fastify.patch('/:id', { ...UpdateBlogSchema }, blogsController.updateData);
     fastify.delete('/:id', { ...DeleteBlogSchema }, blogsController.deleteData);
 }
