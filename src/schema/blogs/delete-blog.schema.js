a228809f7ec96fd4af81d1004e1ee00471833ec0
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-04 23:35:45 +0500

    delete and update schemas were updated successfully

--- src/schema/blogs/delete-blog.schema.js
+++ src/schema/blogs/delete-blog.schema.js
@@ -1,5 +1,31 @@
 export const DeleteBlogSchema = {
     schema: {
         tags: ['Blogs'],
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    statusCode: { type: 'integer', default: 200 },
+                    message: { type: 'string', default: 'OK' },
+                },
+            },
+            404: {
+                type: 'object',
+                properties: {
+                    statusCode: { type: 'integer', default: 404 },
+                    message: { type: 'string', default: 'Not found' },
+                },
+            },
+            500: {
+                type: 'object',
+                properties: {
+                    statusCode: { type: 'integer', default: 500 },
+                    message: {
+                        type: 'string',
+                        default: 'Interval Server Error',
+                    },
+                },
+            },
+        },
     },
 };
--- src/schema/blogs/update-blog.schema.js
+++ src/schema/blogs/update-blog.schema.js
@@ -1,5 +1,31 @@
 export const UpdateBlogSchema = {
     schema: {
         tags: ['Blogs'],
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    statusCode: { type: 'integer', default: 200 },
+                    message: { type: 'string', default: 'OK' },
+                },
+            },
+            404: {
+                type: 'object',
+                properties: {
+                    statusCode: { type: 'integer', default: 404 },
+                    message: { type: 'string', default: 'Not Found' },
+                },
+            },
+            500: {
+                type: 'object',
+                properties: {
+                    statusCode: { type: 'integer', default: 500 },
+                    message: {
+                        type: 'string',
+                        default: 'Interval Server Error',
+                    },
+                },
+            },
+        },
     },
 };
