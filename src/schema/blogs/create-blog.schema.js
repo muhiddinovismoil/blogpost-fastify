6076e653fcd663e353274553c3b4a1a2f39b4a84
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-04 23:08:46 +0500

    create blog schma successfully completed

--- src/schema/blogs/create-blog.schema.js
+++ src/schema/blogs/create-blog.schema.js
@@ -3,8 +3,32 @@ export const CreateBlogSchema = {
         tags: ['Blogs'],
         body: {
             type: 'object',
-            required: ['title', 'subTitle'],
-            properties: {},
+            required: ['title', 'subTitle', 'content'],
+            properties: {
+                title: { type: 'string' },
+                subTitle: { type: 'string' },
+                media: { type: 'string' },
+                content: { type: 'string' },
+            },
+        },
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    statusCode: { type: 'integer', default: 200 },
+                    message: { type: 'string' },
+                    data: {
+                        id: { type: 'string' },
+                    },
+                },
+            },
+            500: {
+                type: 'object',
+                properties: {
+                    statusCode: { type: 'integer', default: 500 },
+                    message: { type: 'string' },
+                },
+            },
         },
     },
 };
