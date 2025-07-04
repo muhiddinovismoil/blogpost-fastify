c077847f46fa10591fa3ae964599ae85b0b42f6c
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-04 21:15:03 +0500

    new schema were indexed

--- src/schema/users/index.js
+++ src/schema/users/index.js
@@ -4,3 +4,4 @@ export * from './send-otp.schema.js';
 export * from './forget-pass.schema.js';
 export * from './reset-pass.schema.js';
 export * from './verify-otp.schema.js';
+export * from './getall-users.schema.js';
