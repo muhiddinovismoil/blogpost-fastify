1359b5f52e4dd512fb450b0de9917619cbed49cb
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-04 22:49:25 +0500

    create blog schema updating

--- src/schema/blogs/create-blog.schema.js
+++ src/schema/blogs/create-blog.schema.js
@@ -1,5 +1,10 @@
 export const CreateBlogSchema = {
     schema: {
         tags: ['Blogs'],
+        body: {
+            type: 'object',
+            required: [],
+            properties: {},
+        },
     },
 };
