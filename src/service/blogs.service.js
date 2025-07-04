2b3058b82bbc23c02ab999522f75af9c1444ca79
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-04 22:18:27 +0500

    blog service successfully finished and ready to test

--- src/service/blogs.service.js
+++ src/service/blogs.service.js
@@ -1,29 +1,54 @@
 export async function getAll(prisma) {
     try {
+        const data = await prisma.blogs.findMany();
+        return data.length == 0 ? [] : data;
     } catch (error) {
         throw new Error(error.message);
     }
 }
 export async function getById(prisma, id) {
     try {
+        const data = await prisma.blogs.findFirst({
+            where: { id },
+        });
+        return data;
     } catch (error) {
         throw new Error(error.message);
     }
 }
 export async function createBlogs(prisma, payload) {
     try {
+        const data = await prisma.blogs.create({
+            data: {
+                ...payload,
+            },
+        });
+        return data.id;
     } catch (error) {
         throw new Error(error.message);
     }
 }
 export async function updateBlogs(prisma, id) {
     try {
+        const data = await getById(id);
+        if (!data) throw new Error('Blog not found');
+        await prisma.blogs.update({
+            where: { id },
+            data: { ...payload },
+        });
+        return 'Blog updated successfully';
     } catch (error) {
         throw new Error(error.message);
     }
 }
 export async function deleteBlogs(prisma, id) {
     try {
+        const data = await getById(id);
+        if (!data) throw new Error('Blog not found');
+        await prisma.blogs.delete({
+            where: { id },
+        });
+        return 'User deleted successfully';
     } catch (error) {
         throw new Error(error.message);
     }
