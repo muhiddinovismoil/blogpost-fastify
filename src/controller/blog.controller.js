d97763946ed14e432e8483ea95502fb4251bcb96
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-05 11:24:22 +0500

    status codes updated on controller

--- src/controller/blog.controller.js
+++ src/controller/blog.controller.js
@@ -5,18 +5,19 @@ import {
     getById,
     updateBlogs,
 } from '../service/blogs.service.js';
+import { HttpStatusCodes } from '../utils/index.js';
 
 export async function getAllBlogs(req, res) {
     try {
         const data = await getAll(req.server.prisma);
-        return res.status(200).send({
-            statusCode: 200,
+        return res.status(HttpStatusCodes.OK).send({
+            statusCode: HttpStatusCodes.OK,
             message: 'Blogs fetched successfully',
             data: data,
         });
     } catch (error) {
-        return res.status(500).send({
-            statusCode: 500,
+        return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({
+            statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
             message: error.message,
         });
     }
@@ -25,14 +26,14 @@ export async function getAllBlogs(req, res) {
 export async function getBlogById(req, res) {
     try {
         const data = await getById(req.server.prisma, req.params.id);
-        return res.status(200).send({
-            statusCode: 200,
+        return res.status(HttpStatusCodes.OK).send({
+            statusCode: HttpStatusCodes.OK,
             message: 'Blogs fetched successfully',
             data: data,
         });
     } catch (error) {
-        return res.status(500).send({
-            statusCode: 500,
+        return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({
+            statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
             message: error.message,
         });
     }
@@ -42,14 +43,14 @@ export async function createData(req, res) {
     try {
         const payload = { ...req.body, userId: req.user.id };
         const data = await createBlogs(req.server.prisma, payload);
-        return res.status(200).send({
-            statusCode: 200,
+        return res.status(HttpStatusCodes.OK).send({
+            statusCode: HttpStatusCodes.OK,
             message: 'Mew  blog created',
             data: { id: data },
         });
     } catch (error) {
-        return res.status(500).send({
-            statusCode: 500,
+        return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({
+            statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
             message: error.message,
         });
     }
@@ -63,16 +64,19 @@ export async function updateData(req, res) {
             req.body
         );
         if (data == 'Blog updated successfully') {
-            return res.status(200).send({ statusCode: 200, message: data });
+            return res
+                .status(HttpStatusCodes.OK)
+                .send({ statusCode: HttpStatusCodes.OK, message: data });
         }
     } catch (error) {
         if (error.message == 'Blog not found') {
-            return res
-                .status(404)
-                .send({ statusCode: 404, message: error.message });
+            return res.status(HttpStatusCodes.NOT_FOUND).send({
+                statusCode: HttpStatusCodes.NOT_FOUND,
+                message: error.message,
+            });
         }
-        return res.status(500).send({
-            statusCode: 500,
+        return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({
+            statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
             message: error.message,
         });
     }
@@ -82,16 +86,19 @@ export async function deleteData(req, res) {
     try {
         const data = await deleteBlogs(req.server.prisma, req.params.id);
         if (data == 'User deleted successfully') {
-            return res.status(200).send({ statusCode: 200, message: data });
+            return res
+                .status(HttpStatusCodes.OK)
+                .send({ statusCode: HttpStatusCodes.OK, message: data });
         }
     } catch (error) {
         if (error.message == 'Blog not found') {
-            return res
-                .status(404)
-                .send({ statusCode: 404, message: error.message });
+            return res.status(HttpStatusCodes.NOT_FOUND).send({
+                statusCode: HttpStatusCodes.NOT_FOUND,
+                message: error.message,
+            });
         }
-        return res.status(500).send({
-            statusCode: 500,
+        return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({
+            statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
             message: error.message,
         });
     }
