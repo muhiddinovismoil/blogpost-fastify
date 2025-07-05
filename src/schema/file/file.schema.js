5783fedd8a440a5874d948964e6347bb6ed05d7b
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-05 11:42:17 +0500

    file upload schma updated

--- src/schema/file/file.schema.js
+++ src/schema/file/file.schema.js
@@ -1,5 +1,47 @@
+import { HttpStatusCodes } from '../../utils/index.js';
+
 export const UploadMediaSchema = {
     schema: {
         tags: ['Files'],
+        consumes: ['multipart/form-data'],
+        body: {
+            type: 'object',
+            properties: {
+                file: {
+                    type: 'string',
+                    format: 'binary',
+                },
+            },
+            required: ['file'],
+        },
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    statusCode: {
+                        type: 'integer',
+                        default: HttpStatusCodes.OK,
+                    },
+                    message: { type: 'string' },
+                    data: {
+                        type: 'object',
+                        properties: {
+                            message: { type: 'string' },
+                            url: { type: 'string' },
+                            fileName: { type: 'string' },
+                        },
+                    },
+                },
+            },
+            500: {
+                type: 'object',
+                properties: {
+                    statusCode: {
+                        type: 'integer',
+                        default: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+                    },
+                },
+            },
+        },
     },
 };
