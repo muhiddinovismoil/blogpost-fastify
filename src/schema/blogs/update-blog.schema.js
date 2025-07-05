0f44f2be153b0deab53cda62f6936066567c9968
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-07-05 11:09:22 +0500

    blog update schema successfully update

--- src/schema/blogs/update-blog.schema.js
+++ src/schema/blogs/update-blog.schema.js
@@ -1,3 +1,5 @@
+import { HttpStatusCodes } from '../../utils/index.js';
+
 export const UpdateBlogSchema = {
     schema: {
         tags: ['Blogs'],
@@ -14,24 +16,33 @@ export const UpdateBlogSchema = {
             200: {
                 type: 'object',
                 properties: {
-                    statusCode: { type: 'integer', default: 200 },
+                    statusCode: {
+                        type: 'integer',
+                        default: HttpStatusCodes.OK,
+                    },
                     message: { type: 'string', default: 'OK' },
                 },
             },
             404: {
                 type: 'object',
                 properties: {
-                    statusCode: { type: 'integer', default: 404 },
+                    statusCode: {
+                        type: 'integer',
+                        default: HttpStatusCodes.NOT_FOUND,
+                    },
                     message: { type: 'string', default: 'Not Found' },
                 },
             },
             500: {
                 type: 'object',
                 properties: {
-                    statusCode: { type: 'integer', default: 500 },
+                    statusCode: {
+                        type: 'integer',
+                        default: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+                    },
                     message: {
                         type: 'string',
-                        default: 'Interval Server Error',
+                        default: 'Internal Server Error',
                     },
                 },
             },
